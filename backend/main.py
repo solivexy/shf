9d60316077e8b58ab291854e4dc530d5a6e1d8a5
NFLSTY <naufalsatya10@gmail.com>
2026-07-16 22:06:01 +0700

    fix: update CORS middleware regex to use raw string for better readability

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8000", "*"],
-    allow_origin_regex="http://(localhost|127\.0\.0\.1)(:[0-9]+)?",
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:[0-9]+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
